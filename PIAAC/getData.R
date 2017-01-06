99e329fb67f177f3226e47e1f9d95572b2631202
Petzi <peter.baumgartner@donau-uni.ac.at>
2017-01-06 16:32:53 +0100

    Gettig data from the OECD Website

--- PIAAC/getData.R
+++ PIAAC/getData.R
@@ -10,6 +10,8 @@ setwd(paste0(getwd(),"/DATA"))
 ###     read.spss() of "foreign" package brought up two warnings
 ###     it seems that the implementation is pretty old
 
+
+# getting extended PUF file about Austria from Statistik Austria
 library(memisc)
 temp <- tempfile()
 download.file(
@@ -25,3 +27,28 @@ write.csv(data, "piaacAustria.csv")
 setwd("../") # go back to PIACC directory
 ## NOTE: huge data.set is very unhandy
 ## develop research question and check the codebook for relevant variables!
+
+# Getting all data files from OECD website
+# Code example at StackOverflow:
+# http://stackoverflow.com/questions/33790052/download-all-files-from-a-folder-on-a-website
+
+setwd("/Users/petzi/Documents/_PB-Data/Programming/R/Learning-R/PIAAC")
+myDir <- getwd()
+if (!dir.exists("./DATA/OECD")) {dir.create("./DATA/OECD")}
+setwd(paste0(getwd(),"/DATA/OECD"))
+## oecd url
+oecdUrl <- "http://vs-web-fs-1.oecd.org/piaac/puf-data/CSV/"
+## base url
+baseUrl <- "http://vs-web-fs-1.oecd.org"
+## query the url to get all the file names ending in '.csv'
+myUrls <- XML::getHTMLLinks(
+        oecdUrl,
+        xpQuery = "//a/@href['.csv'=substring(., string-length(.) - 3)]"
+)
+## create all the new files
+myFiles <- basename(myUrls)
+file.create(myFiles)
+## download them all
+lapply(paste0(oecdUrl,myFiles), function(x) download.file(x, basename(x)))
+## reset working directory to original
+setwd(myDir)
