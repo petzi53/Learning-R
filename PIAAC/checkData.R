856d916040638b2e62b3833fe0d098febb23005a
Petzi <peter.baumgartner@donau-uni.ac.at>
2017-01-07 20:00:58 +0100

    Comparison dplyr vs data.table
    Playing around to work with data

--- PIAAC/checkData.R
+++ PIAAC/checkData.R
@@ -10,10 +10,51 @@ library("data.table")
 f1 <- fread("piaacAustria.csv")
 f2 <- fread("piaacOECD.csv")
 dim(f1); dim(f2)
+f1[, gender_r, j_q08]
+### f1[, .(Geschlecht = f1 %>% count(gender_r), by = .(j_q08)) ]
 
-library(dplyr) # but how to do with data.table?
+library("dplyr")
+f1 <- tbl("piaacAustria.csv")
 tally(group_by(f1, gender_r))
 f1 %>% count(gender_r)
+f1 %>% count(j_q08)
+
+###############################################################################
+
+# ----------------------       data.table vs. dplyr        ---------------------
+
+# ---------------------- (1) data.table ----------------------
+# data.table version of iris: DT
+DT <- as.data.table(iris)
+
+# Group the specimens by Sepal area (to the nearest 10 cm2)
+# and count how many occur in each group
+# and name the output columns `Area` and `Count`
+DT[, .(Count <- .N), by = .(Area = 10 * round(Sepal.Length * Sepal.Width / 10))]
+
+# ---------------------- (2) dplyr --------------------------
+### # tbl (dplyr) version of iris:
+TBL <- as.tbl(iris)
+
+# Group the specimens by Sepal area (to the nearest 10 cm2)
+# and count how many occur in each group
+# and name the output columns `Area` and `Count`
+count(TBL, "Area" = 10 * round(Sepal.Length * Sepal.Width / 10))
+
+# -----   FAZIT: I like dplyr more, it is easier and more intuitive>!  --------
+
+###############################################################################
+
+# ---------------------------------------------------------------------------
+
+myTbl <- as.tbl(iris)
+class(myTbl)
+by_area <- group_by(myTbl, 10 * round(Sepal.Length * Sepal.Width / 10))
+by_area
+x <- transmute(myTbl, area = 10 * round(Sepal.Length * Sepal.Width / 10))
+count(group_by(myTbl, area = 10 * round(Sepal.Length * Sepal.Width / 10)))
+filter(myTbl, by_area)
+
 
 ### the oecd file has 2 cols more than the "extended" Austrian version
 ### [A]: What are these two columns? How to detect the difference?
