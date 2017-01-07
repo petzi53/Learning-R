7ed014e54915158744f4c1c6507124dc0a09879d
Petzi <peter.baumgartner@donau-uni.ac.at>
2017-01-07 23:08:08 +0100

    Starting some data maniluation with dplyr
    - read_csv
    - colnames
    - group_by
    - summarise
    - delete some lines with data.table

--- PIAAC/checkData.R
+++ PIAAC/checkData.R
@@ -6,18 +6,37 @@
 ###  - na values are "NA"
 ###  - gender_r is not 1 and 2 but MALE and FEMALE
 
-library("data.table")
-f1 <- fread("piaacAustria.csv")
-f2 <- fread("piaacOECD.csv")
-dim(f1); dim(f2)
-f1[, gender_r, j_q08]
-### f1[, .(Geschlecht = f1 %>% count(gender_r), by = .(j_q08)) ]
-
 library("dplyr")
-f1 <- tbl("piaacAustria.csv")
-tally(group_by(f1, gender_r))
-f1 %>% count(gender_r)
-f1 %>% count(j_q08)
+library("readr")
+f1 <- read_csv("piaacAustria.csv")       # read the file as tbl into memory
+class(f1)                                # = tbl_df, tbl, data.frame
+col.spec <- spec_csv("piaacAustria.csv") # read the specs of the columns
+col.spec[1]$cols$gender_r                # get type of column "gender_r
+col.names <- colnames(f1)                #  get char vector of column names
+tally(group_by(f1, gender_r))            # counts obs (gender) by group
+f1 %>% count(gender_r)                   # counts obs by group, the same as bevor
+f1 %>% count(j_q08)                      # more interesting: no. of owned books
+by_gender <- group_by(f1, gender_r)      # group data by gender
+by_gender %>% count(j_q08)               # owned book by gender
+group_by(f1, gender_r) %>% count(j_q08)  # the same with shortened syntax
+
+by.books <- f1 %>% group_by((j_q08))
+# colnames(by.books$j_q08) <- "books"
+by.books %>% summarise(n = n(),
+                "%" = round(n() / nrow(by.books) * 100, digits = 2))
+books.owned <- by.books %>% summarise(n = n(),
+                "%" = round(n() / nrow(by.books) * 100, digits = 2))
+# colnames(books.owned)[1] <- "Books"                         # just one column
+# colnames(books.owned)[c(1, 3)] <- c("Books", "Perc.")       # some columns
+colnames(books.owned) <- c("Books", "N", "Percent")       # all columns
+books.owned
+# -----------------------------------------------------------------------------
+# books owned by gender
+books.by.gender <- f1 %>% group_by(gender_r, j_q08)
+books.by.gender %>% summarise(n = n(),
+                       "%" = round(n() / nrow(by.books) * 100, digits = 2))
+
+
 
 ###############################################################################
 
