b48338d442aab22c15e5029faa81163461c40612
petzi53 <peter.baumgartner@donau-uni.ac.at>
2017-01-17 20:52:20 +0100

    Intermediate result

---
+++ Coursera/myTools.R
@@ -0,0 +1,20 @@
+# how to store output from the console?
+# http://stackoverflow.com/questions/3503811/is-there-an-r-equivalent-to-the-bash-command-more
+more <- function(x) {
+        file <- tempfile()
+        sink(file); on.exit(sink())
+        print(x)
+        file.show(file, delete.file = T)
+}
+# ## example of usage
+# more(mtcars)
+# more(more)
+
+# t <- t(vapply(
+#         R.data.core$info[[2]],
+#         str_split_fixed,
+#         c("Name" = "", "Description" = ""),
+#         pattern = " ",
+#         n = 2,
+#         USE.NAMES = FALSE
+# ))
